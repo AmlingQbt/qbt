df84c483455e09135bee66cb3c2d4bfbc940b1b2
Keith Amling <keith.amling@gmail.com>
2015-10-11 20:59:49 -0700

    Really crappy SimpleLocalPinsRepo

---
+++ app/main/src/qbt/pins/SimpleLocalPinsRepo.java
@@ -0,0 +1,42 @@
+package qbt.pins;
+
+import com.google.common.base.Function;
+import java.nio.file.Path;
+import org.apache.commons.lang3.ObjectUtils;
+import qbt.PackageTip;
+import qbt.QbtUtils;
+import qbt.VcsVersionDigest;
+import qbt.repo.RemoteRepoAccessor;
+import qbt.vcs.CacheDirCachedRemoteVcs;
+import qbt.vcs.CachedRemote;
+import qbt.vcs.CachedRemoteVcs;
+import qbt.vcs.RawRemoteVcs;
+
+public final class SimpleLocalPinsRepo extends AbstractLocalPinsRepo {
+    private final CachedRemoteVcs vcs;
+    private final Path root;
+
+    public SimpleLocalPinsRepo(RawRemoteVcs rawRemoteVcs, Path root) {
+        this.vcs = new CacheDirCachedRemoteVcs(rawRemoteVcs, root.resolve("remotes"));
+        this.root = root;
+    }
+
+    @Override
+    public RemoteRepoAccessor findPin(PackageTip repo, VcsVersionDigest version) {
+        Path cache = root.resolve("pins").resolve(repo.pkg);
+
+        QbtUtils.semiAtomicDirCache(cache, "", new Function<Path, ObjectUtils.Null>() {
+            @Override
+            public ObjectUtils.Null apply(Path cacheTemp) {
+                vcs.getRawRemoteVcs().getLocalVcs().createCacheRepo(cacheTemp);
+                return ObjectUtils.NULL;
+            }
+        });
+
+        if(!vcs.getRawRemoteVcs().getLocalVcs().getRepository(cache).commitExists(version)) {
+            return null;
+        }
+
+        return new RemoteRepoAccessor(new CachedRemote(cache.toAbsolutePath().toString(), vcs), version);
+    }
+}
